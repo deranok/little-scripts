8bc85561270839144e83e1788ac7da121ead2aa9
deranok <oppeyfacebook@gmail.com>
2023-08-05 05:50:37 +0000

    Update backup_password.sh

--- backup_password.sh
+++ backup_password.sh
@@ -2,5 +2,5 @@
 cur_d=$(pwd);
 cd "/path/to/password";
 gpg -c passwords
-thunderbird -compose "from='oppeyjeff@gmail.com',to='oppeyjeff@gmail.com',subject='test',body='test',attachment='/home/jeff/Documents/passwords.gpg'"
+thunderbird -compose "from='you@email.com',to='you@email.com',subject='test',body='test',attachment='/path/to/password/passwords.gpg'"
 cd $cur_d;
